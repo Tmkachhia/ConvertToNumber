74267106b51446a6303bb329eb9d5c33d3b08204
Tmkachhia <kachhiatarjani@gmail.com>
2019-12-16 20:39:45 +0000

    homework done

---
+++ src/convertTonumber.java
@@ -0,0 +1,14 @@
+public class convertTonumber {
+    public static int convertTonumber(String value) {
+        return Integer.parseInt(value);
+    }
+        public static void main (String [] args){
+            int a = convertTonumber( "10");
+            int b = convertTonumber("20");
+
+        System.out.println(a*b);
+
+        }
+    }
+
+    
